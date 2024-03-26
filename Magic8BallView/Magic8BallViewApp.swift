ba0495c1ae7edb27e5230294110a65fe7801f8ba
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-26 19:18:33 +1100

    Initial Commit

---
+++ Magic8BallView/Magic8BallViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallViewApp.swift
+//  Magic8BallView
+//
+//  Created by Sidak Singh Aulakh on 26/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
